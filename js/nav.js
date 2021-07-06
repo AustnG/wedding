aac6addc9a738f842081c924c134dd6d6daad0ca
Austin <austin.greer6@gmail.com>
2021-07-05 23:11:29 -0500

    Major nav & styling updates

--- js/nav.js
+++ js/nav.js
@@ -5,3 +5,26 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
+
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".nav-menu");
+const menuItem = document.querySelectorAll(".nav-item");
+
+// main toggle
+menuBtn.addEventListener("click", () => {
+  toggle();
+});
+
+// toggle on item click if open
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (menuBtn.classList.contains("open")) {
+      toggle();
+    }
+  });
+});
+
+function toggle() {
+  menuBtn.classList.toggle("open");
+  menuItems.classList.toggle("open");
+}
